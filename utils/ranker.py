1198756f20297c27d104ddd3f96ca8392fe1e244
Engr-farrukh-khan <farrukhnoorkhan456@gmail.com>
2025-07-05 03:12:57 +0500

    chnging end

--- utils/ranker.py
+++
@@ -1,6 +0,0 @@
-from sklearn.metrics.pairwise import cosine_similarity
-import numpy as np
-
-def rank_resume(jd_embedding, resume_embedding):
-    score = cosine_similarity([jd_embedding], [resume_embedding])[0][0]
-    return float(score) * 100
