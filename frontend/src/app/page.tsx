7460debc76ff8cc5632793da08e7baaa09b42004
Engr-farrukh-khan <farrukhnoorkhan456@gmail.com>
2025-07-04 17:40:33 +0500

    feat: replace default homepage with co custom UI

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,6 +2,12 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-900 text-white text-2xl">
       <h1>🚀 Welcome to CV Ranker by Farrukh Noor Khan</h1>
+      <form action="/api/upload" method="POST" encType="multipart/form-data">
+  <input type="file" name="resume" accept=".pdf" />
+  <button type="submit">Upload</button>
+</form>
+
     </div>
+    
   );
 }
